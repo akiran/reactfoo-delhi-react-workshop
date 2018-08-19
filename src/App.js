cb5558b9219d3694934e546c6b082429b3424754
kiran <kiran@neostack.com>
2018-08-19 12:46:10 +0530

    Title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Logo from "./components/Logo";
+import Title from "./components/Title";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Logo />
+        <Title />
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class Title extends React.Component {
+  validateForm() {}
+  render() {
+    return <div>React Store</div>;
+  }
+}
