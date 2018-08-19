7afb85b7f54c7efac507e15b7691d29b37c09e6b
kiran <kiran@neostack.com>
2018-08-19 13:37:51 +0530

    Product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,25 @@
 import React from "react";
-
-const products = [
-  { id: 1, title: "laptop", price: "$1000", description: "" },
-  { id: 2, title: "keyboard", price: "$100", description: "" }
-];
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        { id: 1, title: "laptop", price: "$1000", description: "" },
+        { id: 2, title: "keyboard", price: "$100", description: "" }
+      ]
+    };
+  }
   render() {
     return (
       <div>
         <h1>Products</h1>
+        <ListGroup>
+          {this.state.products.map(product => (
+            <ListGroupItem key={product.id}>{product.title}</ListGroupItem>
+          ))}
+        </ListGroup>
       </div>
     );
   }
